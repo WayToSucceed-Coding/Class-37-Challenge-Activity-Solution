b9c076452c14f21156be5bf6f9fe792640b1b178
WayToSucceed-Coding <aastha01@waytosucceedclasses.com>
2025-07-01 23:10:47 +0530

    Update challenge_activity.py

--- challenge_activity.py
+++ challenge_activity.py
@@ -5,30 +5,36 @@ from datetime import datetime
 
 log_file = "spy_log.txt"
 
-with open(log_file, "a") as f:
-    f.write(f"=== Folder Spy Log Started ===\n")
-    f.write(f"Started at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n")
+f= open(log_file, "a")
+f.write(f"=== Folder Spy Log Started ===\n")
+f.write(f"Started at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n\n")
+f.close()
 
 class SpyHandler(FileSystemEventHandler):
     
     def on_created(self, event):
+        self.log_event("Created", event.src_path)
         print(f"Created: {event.src_path}")
 
     def on_deleted(self, event):
+        self.log_event("Deleted", event.src_path)
         print(f"Deleted: {event.src_path}")
     
     def on_modified(self, event):
+        self.log_event("Modified", event.src_path)
         print(f"Modified: {event.src_path}")
 
     def on_moved(self, event):
+        self.log_event("Moved", event.src_path)
         print(f"Moved: {event.src_path} to {event.dest_path}")
     
     def on_any_event(self, event):
         print("Hey! Something happened!")
 
     def log_event(self, event_type, path):
-        with open(log_file, "a") as f:
-            f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} EVENT : {event_type} | PATH: {path}\n")
+        f= open(log_file, "a")
+        f.write(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} EVENT : {event_type} | PATH: {path}\n")
+        f.close()
 
 spy_handler = SpyHandler() 
 
